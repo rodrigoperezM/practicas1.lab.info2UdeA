3d224bcbd8b78ca3369bc35c6d3bd5791073d8ec
rodrigoperezM <96254619+rodrigoperezM@users.noreply.github.com>
2024-03-04 16:36:07 -0500

    Add files via upload

---
+++ ejer13inf2UdeA.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// Función para verificar si un número es primo
+bool Primo(int n) 
+{
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; ++i) //aumenta iteracion
+	{
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() 
+{
+
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+
+    int suma = 0;
+    for (int i = 2; i < num; ++i) 
+	{
+        if (Primo(i)) 
+		{
+            suma += i;//cuenta la suma de los números primos encontrados en la iteración.
+
+
+
+
+
+
+        }
+    }
+
+    cout << "El resultado de la suma es: " << suma << "." << endl;
+
+    return 0;
+}
